d4c93bab5b1e25217560ca301706bf87d9cabd88
Keith Gabryelski <keith@fiksu.com>
2012-04-04 18:30:48 -0400

    blank rails app ready for coding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScriptScheduler::Application.config.secret_token = 'e9fd0f3187d8317b45821025ba4bd01ae3430beaef1154b1bb1687963498ca77453ccbbe77426a3b6b4a8dafbe4c4a0eab1b166e143523ae271430580bcf6a6c'
