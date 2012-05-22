c6ce397133bd1d60e30f46abcdaf209a0ea7c87d
Keith Gabryelski <keith@fiksu.com>
2012-05-21 22:16:23 -0400

    use Af::Application

--- app/models/process/naf/application.rb
+++ app/models/process/naf/application.rb
@@ -1,5 +1,11 @@
 module Process::Naf
-  class Application < Af::DaemonProcess
+  class Application < ::Af::Application
+    opt :naf_application_id, :env => "NAF_APPLICATION_ID", :type => :int, :default => "unknown"
+
+    def log4r_name_suffix
+      return ":[#{@naf_application_id}]"
+    end
+
     def work
     end
   end
