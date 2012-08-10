65789040babea82820e10815008024c57c58cb3c
Nathaniel Lim <nathaniel.j.lim@gmail.com>
2012-08-10 13:58:48 -0400

    added some validations to the models, for good error messages in the UI

--- app/models/naf/application_schedule_affinity_tab.rb
+++ app/models/naf/application_schedule_affinity_tab.rb
@@ -2,6 +2,8 @@ module Naf
   class ApplicationScheduleAffinityTab < NafBase
     validates :application_schedule_id, :affinity_id, :presence => true
 
+    validates_uniqueness_of :affinity_id, :scope => :application_schedule_id, :message => "has already been taken for this script"
+
     belongs_to :application_schedule, :class_name => '::Naf::ApplicationSchedule'
     belongs_to :affinity, :class_name => '::Naf::Affinity'
 
--- app/models/naf/machine_affinity_slot.rb
+++ app/models/naf/machine_affinity_slot.rb
@@ -2,6 +2,8 @@ module Naf
   class MachineAffinitySlot < NafBase
     validates :machine_id, :affinity_id, :presence => true
 
+    validates_uniqueness_of :affinity_id, :scope => :machine_id, :message => "has been taken for this machine"
+
     belongs_to :machine, :class_name => '::Naf::Machine'
     belongs_to :affinity, :class_name => '::Naf::Affinity'
 
