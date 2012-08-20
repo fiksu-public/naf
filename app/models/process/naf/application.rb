aea9d70c68a3e0d8f2155831511d11a0c2f6b051
Keith Gabryelski <keith@fiksu.com>
2012-08-20 15:37:31 -0400

    request to terminate function

--- app/models/process/naf/application.rb
+++ app/models/process/naf/application.rb
@@ -6,6 +6,22 @@ module Process::Naf
       return ":[#{@naf_application_id}]"
     end
 
+    def requested_to_terminate?
+      if @naf_application_id.is_a? Integer && @naf_application_id > 0
+        job = ::Naf::Job.find(@naf_application_id)
+        if job
+          if job.request_to_terminate
+            return true
+          end
+          # if no machines row exist assume someone is running by hand (do not terminate)
+          if job.machine && !job.machine.enabled
+            return true
+          end
+        end
+      end
+      return false
+    end
+
     def work
     end
   end
