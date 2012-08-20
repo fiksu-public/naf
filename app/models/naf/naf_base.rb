b20b12dc8d8d701856068e0057cf31230a335df3
Nathaniel Lim <nathaniel.j.lim@gmail.com>
2012-08-20 13:28:25 -0400

    Isolated migrations, configurable database choice

--- app/models/naf/naf_base.rb
+++ app/models/naf/naf_base.rb
@@ -1,8 +1,30 @@
 module Naf
   class NafBase < ActiveRecord::Base
+    
+
+    self.abstract_class = true
+
+    def self.reset_naf_connection
+      self.connection.disconnect!
+      self.establish_connection "warehousing_#{Rails.env}"
+    end
+
+
+
     def self.full_table_name_prefix
       "#{JOB_SYSTEM_SCHEMA_NAME}."
     end
-    self.abstract_class = true
+    
+    def self.naf_environment
+      naf_environments = ActiveRecord::Base.configurations.keys.select{|env| env == "naf_#{Rails.env}"}
+      if naf_environments.any?
+        return naf_environments.first
+      else
+        return Rails.env.to_s
+      end
+    end
+
+    establish_connection NafBase.naf_environment
+
   end
 end
