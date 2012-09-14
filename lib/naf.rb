1fecd9906cee71930ff0e5feed79c11ebc4310c6
Nathaniel Lim <nathaniel.j.lim@gmail.com>
2012-09-14 11:19:28 -0400

    Access to port set

--- lib/naf.rb
+++ lib/naf.rb
@@ -35,6 +35,10 @@ module Naf
       configuration.papertrail_group_id
     end
 
+    def papertrail_port
+      configuration.papertrail_port
+    end
+
     def using_another_database?
       model_class != ActiveRecord::Base
     end
--- lib/naf/configuration.rb
+++ lib/naf/configuration.rb
@@ -2,6 +2,7 @@ module Naf
 
   class Configuration
     attr_accessor :schema_name, :model_class, :controller_class, :title, :papertrail_group_id
+    attr_reader :papertrail_port
     
     def initialize
       @model_class = ::ActiveRecord::Base
@@ -12,6 +13,7 @@ module Naf
 
     def papertrail_port=(port)
       ENV["PAPERTRAIL_PORT"] = port.to_s
+      @papertrail_port = port
     end
 
   end
