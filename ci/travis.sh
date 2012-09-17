9fd39089374465681d1a24a50c49b58760771d9f
Nathaniel Lim <nathaniel.j.lim@gmail.com>
2012-09-17 13:21:41 -0400

    call all tasks at once

--- ci/travis.sh
+++ ci/travis.sh
@@ -4,10 +4,7 @@ rake="bundle exec rake"
 
 cd spec/dummy
 
-$rake db:create
-$rake db:migrate
-$rake naf:janitor:infrastructure
-$rake db:test:clone_structure
+$rake db:create db:migrate naf:janitor:infrastructure db:test:clone_structure
 
 cd ../..
 
