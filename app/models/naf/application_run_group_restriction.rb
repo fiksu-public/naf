d487601f66a510cc4b700fab27e68725423ff483
Keith Gabryelski <keith@fiksu.com>
2012-10-01 14:39:17 -0400

    helpers for restrictions

--- app/models/naf/application_run_group_restriction.rb
+++ app/models/naf/application_run_group_restriction.rb
@@ -3,5 +3,17 @@ module Naf
     NO_RESTRICTIONS = 1
     ONE_AT_A_TIME = 2
     ONE_PER_MACHINE = 3
+
+    def self.no_restrictions
+      return find(NO_RESTRICTIONS)
+    end
+
+    def self.one_at_a_time
+      return find(ONE_AT_A_TIME)
+    end
+
+    def self.one_per_machine
+      return find(ONE_PER_MACHINE)
+    end
   end
 end
