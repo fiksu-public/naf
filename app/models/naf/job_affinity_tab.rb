d79a29622c3916b3180cf08e52a70cd1db87f657
Alexander <dembskoi@gmail.com>
2012-11-27 16:30:59 +0400

    fixed bug with partitioned

--- app/models/naf/job_affinity_tab.rb
+++ app/models/naf/job_affinity_tab.rb
@@ -6,14 +6,26 @@ module Naf
 
     belongs_to :affinity, :class_name => "::Naf::Affinity"
 
-    delegate :title, :script_type_name, :command, :to => :job
-
     delegate :affinity_name, :affinity_classification_name, :short_name_if_it_exist, :to => :affinity
 
     attr_accessible :affinity_id
 
     partitioned do |partition|
       partition.foreign_key :affinity_id, full_table_name_prefix + "affinities"
+    end
+
+    def job
+      return ::Naf::Job.
+        from_partition(id).
+        where(:id => job_id).first
+    end
+
+    def script_type_name
+      job.script_type_name
+    end
+
+    def command
+      job.command
     end
   end
 end
