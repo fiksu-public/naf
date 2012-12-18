76d488406797ad0cff1b140ad62311502d129f68
Alexander <dembskoi@gmail.com>
2012-12-18 20:38:25 +0400

    fixed sql formatting

--- app/models/logical/naf/job_statuses/finished_less_minute.rb
+++ app/models/logical/naf/job_statuses/finished_less_minute.rb
@@ -5,11 +5,18 @@ module Logical
 
         def self.all(conditions)
           <<-SQL
-            (SELECT j.*, null AS "job_id"
-              FROM "#{::Naf.schema_name}"."jobs" AS j
-              WHERE j.finished_at > '#{Time.zone.now - 1.minute}'
-              #{conditions}
-              ORDER BY finished_at desc)
+            (
+             SELECT
+               j.*,
+               NULL AS job_id
+              FROM
+               #{::Naf::Job.table_name} AS j
+              WHERE
+               j.finished_at > '#{Time.zone.now - 1.minute}'
+               #{conditions}
+              ORDER BY
+               finished_at DESC
+            )
           SQL
         end
 
