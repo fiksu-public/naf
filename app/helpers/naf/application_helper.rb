57b676dbad8eecb7434da070f292e2117ad58979
Alexander <dembskoi@gmail.com>
2013-02-11 13:32:36 +0400

    added a prefix 'naf' for two method from application_helper

--- app/helpers/naf/application_helper.rb
+++ app/helpers/naf/application_helper.rb
@@ -193,11 +193,11 @@ module Naf
       return url
     end
 
-    def link_to_remove_fields(name, f)
+    def naf_link_to_remove_fields(name, f)
       f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
     end
 
-    def link_to_add_fields(name, f, association)
+    def naf_link_to_add_fields(name, f, association)
       new_object = f.object.class.reflect_on_association(association).klass.new
       fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
         render(association.to_s, :f => builder)
