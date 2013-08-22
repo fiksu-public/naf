bccfb882a3a259aee35b9259bf657f7cfc3dab52
Leonardo Meira <lmeira@fiksu.com>
2013-08-22 15:17:20 -0400

    updated models

--- app/models/naf/application_schedule_prerequisite.rb
+++ app/models/naf/application_schedule_prerequisite.rb
@@ -7,5 +7,30 @@ module Naf
     validates :application_schedule_id, :uniqueness => { :scope => :prerequisite_application_schedule_id }
 
     attr_accessible :application_schedule_id, :prerequisite_application_schedule_id
+
+    def self.pickleables(pickler)
+			return self.joins([application_schedule: :application]).
+				where('applications.deleted = false').
+				where(
+					'NOT EXISTS(
+					   SELECT
+						   1
+						 FROM
+						   naf.application_schedules AS a_s
+						 WHERE
+						   application_schedule_prerequisites.prerequisite_application_schedule_id = a_s.id AND
+						 EXISTS(
+						   SELECT
+							   1
+							 FROM
+							   naf.applications AS a
+							 WHERE
+							   a_s.application_id = a.id AND
+								   deleted IS TRUE
+						 )
+					)'
+				)
+    end
+
   end
 end
