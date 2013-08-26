b1ccd5a9bf7986fc8ff8f7b3262c9b0a1096e9c6
Keith Gabryelski <keith@gabryelski.com>
2013-08-25 21:34:39 -0400

    show enabled runners

--- app/models/naf/machine_runner.rb
+++ app/models/naf/machine_runner.rb
@@ -1,27 +1,25 @@
 module Naf
   class MachineRunner < NafBase
     # Protect from mass-assignment issue
-    attr_accessible :machine_id,
-                    :runner_cwd
+    attr_accessible :machine_id, :runner_cwd
 
     #---------------------
     # *** Associations ***
     #+++++++++++++++++++++
 
-    belongs_to :machine,
-      class_name: '::Naf::Machine'
-    has_many :machine_runner_invocations,
-      class_name: '::Naf::MachineRunnerInvocation'
-    has_many :historical_jobs,
-      class_name: '::Naf::HistoricalJob'
+    belongs_to :machine, class_name: '::Naf::Machine'
+    has_many :machine_runner_invocations, class_name: '::Naf::MachineRunnerInvocation'
+    has_many :historical_jobs, class_name: '::Naf::HistoricalJob'
 
     #--------------------
     # *** Validations ***
     #++++++++++++++++++++
 
-    validates :machine_id,
-              :runner_cwd, presence: true
+    validates :machine_id, :runner_cwd, presence: true
     validates :machine_id, uniqueness: { scope: :runner_cwd }
 
+    def self.enabled
+      return joins(:machine).where("machines.enabled = true")
+    end
   end
 end
