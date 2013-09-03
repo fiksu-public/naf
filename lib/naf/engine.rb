89b1f16bf6b98215b58d77a54bf6e639f3f3fc3e
Leonardo Meira <lmeira@fiksu.com>
2013-09-03 13:49:00 -0400

    updated engine.rb file with dependency

--- lib/naf/engine.rb
+++ lib/naf/engine.rb
@@ -4,13 +4,14 @@ require 'facter'
 require 'jquery-rails'
 require 'log4r_remote_syslog_outputter'
 require 'partitioned'
+require 'd3-rails'
 
 module Naf
   class Engine < ::Rails::Engine
     isolate_namespace Naf
     engine_name "naf"
 
-    initializer "dependencies" do 
+    initializer "dependencies" do
       require 'partitioned'
       require 'log4r_remote_syslog_outputter'
     end
