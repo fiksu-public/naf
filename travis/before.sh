33f163e1a45e4625a5f5e7009565edb8c55329a4
David Hofer <david.hofer@gmail.com>
2015-10-29 18:05:52 -0400

    Run the naf database janitor as part of travis runs.

--- travis/before.sh
+++ travis/before.sh
@@ -4,4 +4,9 @@ cd spec/dummy/
 cp config/database-primary.yml config/database.yml
 bundle exec rake db:create
 bundle exec rake db:migrate
+
+cd ../..
+bundle exec rake app:naf:janitor:infrastructure
+
+cd spec/dummy/
 bundle exec rake db:test:clone_structure
