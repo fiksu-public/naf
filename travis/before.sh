8f21a010cf35a64422c2e8547051454ec8925a5e
David Hofer <david.hofer@gmail.com>
2015-10-29 18:22:05 -0400

    Fix some errors.

--- travis/before.sh
+++ travis/before.sh
@@ -3,9 +3,9 @@
 cd spec/dummy/
 cp config/database-primary.yml config/database.yml
 bundle exec rake db:create
-bundle exec rake db:migrate
 
 cd ../..
+bundle exec rake db:migrate
 bundle exec rake app:naf:janitor:infrastructure
 
 cd spec/dummy/
